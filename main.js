398ab242cb319f32ed141f41c59cc1b945669f59
Afsaneh234 <nina.alaghemand@outlook.com>
2023-01-23 19:50:47 -0800

    my second try for homework 3

--- main.js
+++ main.js
@@ -1,46 +1,61 @@
 
-document.addEventListener("DOMContentLoaded", function (event) {
+    let origArray=[2,3,4];
 
-    let origArray = [2,3,4];
 
-    document.getElementById("square").onclick= function () {
-        let x = origArray.map(mySquare);
+   document.addEventListener("DOMContentLoaded", function (event) {
+
+  
       
-        document.getElementById("input1").value = x[0];
-        document.getElementById("input2").value = x[1];
-        document.getElementById("input3").value = x[2];
-    };
+        document.getElementById("TextBox1").value = origArray[0];
+        document.getElementById("TextBox2").value = origArray[1];
+        document.getElementById("TextBox3").value = origArray[2];
     
-    document.getElementById("cube").addEventListener("click", function () {
+    
+    document.getElementById("button2").addEventListener("click", function () {
         let x = origArray.map(myCube);
-        document.getElementById("input1").value = x[0];
-        document.getElementById("input2").value = x[1];
-        document.getElementById("input3").value = x[2];
+        document.getElementById("TextBox1").value = x[0];
+        document.getElementById("TextBox2").value = x[1];
+        document.getElementById("TextBox3").value = x[2];
         
         
     });
     
-    document.getElementById("toTheFourth").addEventListener("click", function () {
-        let x = origArray.map(myFourthPower)
-       
-        document.getElementById("input1").value = x[0];
-        document.getElementById("input2").value = x[1];
-        document.getElementById("input3").value = x[2];
+    document.getElementById("button3").addEventListener("click", button3);
+
+   
 
 
-    });
 });
 
-function mySquare(oneItem) {
-    oneItem=Math.pow(oneItem,2);
-    return oneItem;
+function button1Function(){
+  
+    let x=origArray.map(square);
+    document.getElementById("TextBox1").value = x[0];
+    document.getElementById("TextBox2").value = x[1];
+    document.getElementById("TextBox3").value = x[2];
+
+
+}
+function button3(){
+    let x=origArray.map(four);
+    document.getElementById("TextBox1").value = x[0];
+    document.getElementById("TextBox2").value = x[1];
+    document.getElementById("TextBox3").value = x[2];
 }
 
-function myCube(oneItem) {
-    return oneItem= Math.pow(oneItem,3);
+function square(item){
+
+    let x= item **2;
+    return x;
 }
 
-function myFourthPower(oneItem) {
-    oneItem = Math.pow(oneItem,4);
-    return oneItem;
+function myCube(item) {
+     let x=item **3;
+     return x;
 }
+
+function four(item) {
+    let x = item ** 4;
+    return x;
+}
+
